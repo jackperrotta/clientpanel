6f4186d8951805b4332601f0f4dc9a6c2d158e98
Vitris <tug24657@temple.edu>
2017-12-18 17:26:28 -0500

    user authentication and authguard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,15 +23,17 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 //Create Routes
 const appRoutes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent, canActivate:[AuthGuard]},
   {path:'register', component: RegisterComponent},
   {path:'login', component: LoginComponent},
-  {path: 'add-client', component: AddClientComponent},
-  {path: 'client/:id', component: ClientDetailsComponent},
-  {path: 'edit-client/:id', component: EditClientComponent}
+  {path: 'add-client', component: AddClientComponent, canActivate:[AuthGuard]},
+  {path: 'client/:id', component: ClientDetailsComponent, canActivate:[AuthGuard]},
+  {path: 'edit-client/:id', component: EditClientComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
@@ -60,7 +62,9 @@ const appRoutes: Routes = [
   providers: [
     AngularFireDatabase,
     AngularFireDatabaseModule,
-    ClientService
+    ClientService,
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean;
+  loggedInUser: string;
+  showRegister: boolean;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessagesService: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth => {
+      if(auth){
+        this.isLoggedIn = true;
+        this.loggedInUser = auth.email;
+      } else{
+        this.isLoggedIn = false;
+      }
+    });
+  }
+
+  onLogoutClick(){
+    this.authService.logout();
+    this.flashMessagesService.show('You are logged out', {
+      cssClass:'alert-success', timeout: 4000
+    });
+    this.router.navigate(['./login']);
   }
 
 }
